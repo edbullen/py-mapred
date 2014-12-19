ca63d403e545aa05cf246246288db415db83d495
Ed Bullen <ed.bullen@gmail.com>
2014-12-19 15:44:33 +0000

    add lsoa_name, bug fixes

--- mapper.py
+++ mapper.py
@@ -4,8 +4,10 @@ import sys
 import re
 import os
 import logging
+import csv
 
 import mapred_shared
+
 
 
 """ Hadoop Mapper
@@ -19,6 +21,8 @@ Crime ID,Month,Reported by,Falls within,Longitude,Latitude,Location,LSOA code,LS
 target is key-value summary where
 KEY = month:lsoa_code
 VALUE = crime
+
+also tag on "lsoa_name" as 3rd element <KEY>    <VALUE>    <lsoa_name>
 
 lsoa_code,      -- input FIELD 8
 month,          -- input FIELD 2  
@@ -48,6 +52,8 @@ pid = str(os.getpid())
 i = 0
 missing = 0
 unclassified = 0
+lsoa_code = ""; last_lsoa = ""
+lsoa_name = ""
 
 # Logging #
 logging.basicConfig(filename='/tmp/mapper.log',
@@ -70,7 +76,11 @@ try:
     for line in sys.stdin:
         i += 1
         line = line.strip()
-        unpacked = line.split(",")
+        
+        for row in csv.reader([line], skipinitialspace=True):
+            unpacked = row
+       
+        #unpacked = line.split(",")
         unpacked = [item.lower() for item in unpacked] # set all strings lowercase
         if i == 1:
             logging.info("| First line is %s", line)
@@ -80,9 +90,14 @@ try:
             date = date.split("-")[0] 
     
         lsoa_code = str(unpacked[7])
-        #lsoa_name = unpacked[8]
+        #if lsoa_code != last_lsoa:  # simple way to make sure lsoa_name is the same for all lsoa  
+        lsoa_name = unpacked[8] # populate each time we start processing new LSOA
+                    
         crime_type = unpacked[9]
         crime = ""
+    
+    
+
     
         #if unpacked[9] includes "bicycle", bicycle_theft = 1
         if re.search("bicycle", crime_type,):
@@ -92,16 +107,14 @@ try:
         elif re.search("burglary", crime_type,): 
             crime = mapred_shared.crimes_list[2]
         elif re.search("criminal", crime_type,):
-            crime = "damage_or_arson"   
+            crime = mapred_shared.crimes_list[3]   
         elif re.search("drugs", crime_type,):
-            crime = mapred_shared.crimes_list[3]
+            crime = mapred_shared.crimes_list[4]            
         elif (re.search("other", crime_type,)) and  (re.search("theft", crime_type,)):
-            crime = mapred_shared.crimes_list[4]
-        elif re.search("weapons", crime_type,):
             crime = mapred_shared.crimes_list[5]
-        elif (re.search("order", crime_type,)) and (re.search("public", crime_type,)):
+        elif re.search("weapons", crime_type,):
             crime = mapred_shared.crimes_list[6]
-        elif re.search("robbery", crime_type,):
+        elif (re.search("order", crime_type,)) and (re.search("public", crime_type,)):
             crime = mapred_shared.crimes_list[7]
         elif re.search("shoplifting", crime_type,):
             crime = mapred_shared.crimes_list[8]
@@ -111,7 +124,7 @@ try:
             crime = mapred_shared.crimes_list[10]
         elif re.search("vehicle", crime_type,):
             crime = mapred_shared.crimes_list[11]
-        elif re.search("sex", crime_type,) :
+        elif (re.search("sex", crime_type,)) or (re.search("violen", crime_type,)) :
             crime = mapred_shared.crimes_list[12]
         elif (re.search("other", crime_type,)) and (re.search("crime", crime_type,)) :
             crime = mapred_shared.crimes_list[13]
@@ -120,10 +133,10 @@ try:
             unclassified += 1
             
         if lsoa_code == "":
-            lsoa_code = "MISSING_DATA"                  #MISSING
+            lsoa_code = "MISSING_LSOA_DATA"                  #MISSING
             crime = mapred_shared.crimes_list[14]  # Crime is "missing data"
         if date == "":
-            date = "MISSING_DATA"                       #MISSING
+            date = "MISSING_DATE_DATA"                       #MISSING
             crime = mapred_shared.crimes_list[14]  # Crime is "missing data"
         if crime == mapred_shared.crimes_list[14]:
             missing += 1
@@ -131,9 +144,12 @@ try:
             
         key = ":".join((date, lsoa_code))
         value = crime
+        last_lsoa = lsoa_code
     
         #printv(key + "\t" + value)
-        mapred_shared.printout(key + "\t" + value)
+        if not lsoa_name:
+            lsoa_name = "NULL"
+        mapred_shared.printout(key + "\t" + value + "\t" + lsoa_name)
             
 except Exception as err:
     listlen = str(len(unpacked))
